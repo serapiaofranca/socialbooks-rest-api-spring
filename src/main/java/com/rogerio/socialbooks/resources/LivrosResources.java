91ca74e824d2c24ccc0d08bd178845e0a6fa992b
Rogerio A Serapiao <rserapa@gmail.com>
2021-03-18 14:52:58 -0300

    Adicionado domain para resource Livros

--- src/main/java/com/rogerio/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/rogerio/socialbooks/resources/LivrosResources.java
@@ -1,15 +1,25 @@
 package com.rogerio.socialbooks.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rogerio.socialbooks.domain.Livro;
 
 @RestController
 public class LivrosResources {
 	
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar() {
-		return "Java for begginers, Git passo-a-passo";
+	public List<Livro> listar() {
+		
+		Livro livro1 = new Livro("Spring Boot Avançaco");
+		Livro livro2 = new Livro("Rest API com Spring MVC");
+		Livro[] livros= {livro1, livro2};
+		
+		return Arrays.asList(livros);
 	}
 
 }
