b10f639c049eca973cafc3a270b492f6ecfcd3f0
Rogerio A Serapiao <rserapa@gmail.com>
2021-03-31 20:19:10 -0300

    personalizando mensagens de erro

--- src/main/java/com/rogerio/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/rogerio/socialbooks/handler/ResourceExceptionHandler.java
@@ -2,20 +2,28 @@ package com.rogerio.socialbooks.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rogerio.socialbooks.domain.DetalhesErro;
 import com.rogerio.socialbooks.services.exception.LivroNaoEncontradoException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler {	
 	
 	@ExceptionHandler(LivroNaoEncontradoException.class)
-	public ResponseEntity<Void> handleLivroNaoEncontradoException( 
+	public ResponseEntity<DetalhesErro> handleLivroNaoEncontradoException( 
 				LivroNaoEncontradoException e, HttpServletRequest request){
 		
-		return ResponseEntity.notFound().build();
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(404L);
+		erro.setTitulo("Livro não encontrado");
+		erro.setMensagemDesenvolvedor("http://erros.rogerio.socialbooks.com/404");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}		
 
 }
